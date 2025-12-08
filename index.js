671d7db47c42ac85e43446b912210bf8f1f4f0a7
mice333 <nokisev@mail.ru>
2025-12-08 23:40:09 +0300

    Tasks in array of objects, and also added functional for add button

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const list = document.querySelector(".task-list");
-const tasks = document.querySelectorAll(".task");
-let id = 0;
-function addTask() {
-  list.insertAdjacentHTML("beforeend", `<div class="task">${id++} task</div>`);
-}
 
+const tasksList = [
+  {
+    id: 0,
+  },
+  {
+    id: 1,
+  },
+  {
+    id: 2,
+  },
+  {
+    id: 3,
+  },
+  {
+    id: 4,
+  },
+];
+
+let id = tasksList.length - 1;
+for (let i = 0; i < id; i++) {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<div class="task">${tasksList[i].id} task</div>`
+  );
+}
+let tasks = document.querySelectorAll(".task");
+
+function addTask() {
+  tasksList.push({ id: id });
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<div class="task">${tasksList[tasksList.length - 1].id} task</div>`
+  );
+  id = tasksList.length - 1;
+  tasks = document.querySelectorAll(".task");
+  tasks.forEach((task) => observer.observe(task));
+}
 const addButton = document.querySelector(".btn");
 addButton.addEventListener("click", addTask);
 
